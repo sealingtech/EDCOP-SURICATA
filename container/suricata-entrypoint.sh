45771b251bbd7c6e052080b5d95e84dff1a85af8
dlohin <daniel.lohin@sealingtech.org>
2018-11-25 18:13:26 -0500

    Update suricata-entrypoint.sh

--- container/suricata-entrypoint.sh
+++ container/suricata-entrypoint.sh
@@ -9,6 +9,9 @@ fi
 
 sleep 10
 
+wget -P / https://repos.dds.io/rules/emerging.rules.tar.gz
+tar xvzf /emerging.rules.tar.gz -C /
+
 
 # Start Suricata normally
 suricata -c /etc/suricata/suricata.yaml --af-packet
