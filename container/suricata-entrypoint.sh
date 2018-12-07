3c4330c6310ada5e7264fd0c3f58a9f65aaf2487
root <root@virtual.edcop.io>
2018-12-07 15:05:59 -0500

    updated interfaces

--- container/suricata-entrypoint.sh
+++ container/suricata-entrypoint.sh
@@ -7,6 +7,10 @@ if [ ! -d "/logs/suricata" ]; then
   mkdir -p /logs/suricata
 fi
 
+sed -i 's/${INTERFACE1}/'$INTERFACE1' /g' /tmp/suricata/suricata.yaml
+sed -i 's/${INTERFACE2}/'$INTERFACE2' /g' /tmp/suricata/suricata.yaml
+
+cp -rpf /tmp/suricata/* /etc/suricata/
 
 # Start Suricata normally
 suricata -c /etc/suricata/suricata.yaml --af-packet
