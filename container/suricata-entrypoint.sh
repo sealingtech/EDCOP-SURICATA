fd0f898c46d986c8e4ed21dcd2a5595388a61939
Daniel Lohin <daniel.lohin@sealingtech.org>
2018-12-07 09:19:48 -0500

    added the ability to modify rules update source.  Can now add rules for enabling/disabling  rules, etc.

--- container/suricata-entrypoint.sh
+++ container/suricata-entrypoint.sh
@@ -7,11 +7,6 @@ if [ ! -d "/logs/suricata" ]; then
   mkdir -p /logs/suricata
 fi
 
-sleep 10
-
-wget -P / http://repos.dds.io/rules/emerging.rules.tar.gz
-tar xvzf /emerging.rules.tar.gz -C /
-
 
 # Start Suricata normally
 suricata -c /etc/suricata/suricata.yaml --af-packet
