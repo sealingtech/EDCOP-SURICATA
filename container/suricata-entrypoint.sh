3f58d80431602d48a16814af4e95f4d214cc0450
root <root@virtual.edcop.io>
2018-12-07 15:53:00 -0500

    fix interface errors

--- container/suricata-entrypoint.sh
+++ container/suricata-entrypoint.sh
@@ -7,10 +7,11 @@ if [ ! -d "/logs/suricata" ]; then
   mkdir -p /logs/suricata
 fi
 
-sed -i 's/${INTERFACE1}/'$INTERFACE1' /g' /tmp/suricata/suricata.yaml
-sed -i 's/${INTERFACE2}/'$INTERFACE2' /g' /tmp/suricata/suricata.yaml
-
 cp -rpf /tmp/suricata/* /etc/suricata/
+
+sed -i 's/${INTERFACE1}/'$INTERFACE1' /g' /etc/suricata/suricata.yaml
+sed -i 's/${INTERFACE2}/'$INTERFACE2' /g' /etc/suricata/suricata.yaml
+
 
 # Start Suricata normally
 suricata -c /etc/suricata/suricata.yaml --af-packet
