0eb9bb4898fb1e79a0804a76979c026a346dccb7
root <root@virtual.edcop.io>
2018-12-10 10:57:34 -0500

    changes to make troubleshooting easier

--- container/suricata-entrypoint.sh
+++ container/suricata-entrypoint.sh
@@ -6,11 +6,15 @@ set -e
 if [ ! -d "/logs/suricata" ]; then
   mkdir -p /logs/suricata
 fi
+echo "=====/tmp/suricata contents======
+ls -la /tmp/suricata
+echo "=====/etc/suricata contents======
+ls -la /etc/suricata
 
-#cp -rpf /tmp/suricata/* /etc/suricata/
+cp -rpf /tmp/suricata/* /etc/suricata/
 
-#sed -i 's/${INTERFACE1}/'$INTERFACE1' /g' /etc/suricata/suricata.yaml
-#sed -i 's/${INTERFACE2}/'$INTERFACE2' /g' /etc/suricata/suricata.yaml
+sed -i 's/${INTERFACE1}/'$INTERFACE1' /g' /etc/suricata/suricata.yaml
+sed -i 's/${INTERFACE2}/'$INTERFACE2' /g' /etc/suricata/suricata.yaml
 
 
 # Start Suricata normally
